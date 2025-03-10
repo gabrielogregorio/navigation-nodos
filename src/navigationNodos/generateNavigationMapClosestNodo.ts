4c07fb72decb76b6bba6d3398d3149179254cc78
Gabriel Gregorio <gabriel.gregorio.1@outlook.com>
2025-03-10 16:17:03 -0300

    refactor: update name consts

--- src/navigationNodos/generateNavigationMapClosestNodo.ts
+++ src/navigationNodos/generateNavigationMapClosestNodo.ts
@@ -2,57 +2,46 @@ import { defaultConfig } from './constants';
 import { ClosestType, ConfigType, MapeClosedType } from './types';
 
 export const generateNavigationMapClosestNodo = (config: ConfigType = defaultConfig) => {
-  const elements = Array.from(document.querySelectorAll(`[${config.atributeControl}]`));
-
+  const elements = [...document.querySelectorAll(`[${config.atributeControl}]`)];
   const mapClosestNodo: MapeClosedType = {};
 
-  elements.forEach((currnetElement) => {
-    const rect = currnetElement.getBoundingClientRect();
-    const nodoId = currnetElement.getAttribute(config.atributeControl)!;
+  elements.forEach((currentElement) => {
+    const rect = currentElement.getBoundingClientRect();
+    const nodoId = currentElement.getAttribute(config.atributeControl)!;
 
-    const closest: ClosestType = {
-      up: null,
-      down: null,
-      left: null,
-      right: null,
-    };
-    const minDistances = {
-      up: Infinity,
-      down: Infinity,
-      left: Infinity,
-      right: Infinity,
-    };
+    const closest: ClosestType = { up: null, down: null, left: null, right: null };
+    const minDistances = { up: Infinity, down: Infinity, left: Infinity, right: Infinity };
 
-    elements.forEach((unknowNodo) => {
-      if (currnetElement === unknowNodo) return;
+    elements.forEach((otherElement) => {
+      if (currentElement === otherElement) return;
 
-      const otherRect = unknowNodo.getBoundingClientRect();
-      const otherId = unknowNodo.getAttribute(config.atributeControl);
+      const otherRect = otherElement.getBoundingClientRect();
+      const otherId = otherElement.getAttribute(config.atributeControl);
 
-      const dxUnknowNodo = otherRect.left - rect.left;
-      const dyUnknowNodo = otherRect.top - rect.top;
-      const distanceUnknowNodo = Math.abs(dxUnknowNodo) + Math.abs(dyUnknowNodo);
+      const dx = otherRect.left - rect.left;
+      const dy = otherRect.top - rect.top;
+      const distance = Math.abs(dx) + Math.abs(dy);
 
-      if (dyUnknowNodo < 0 && minDistances.up > distanceUnknowNodo) {
+      if (dy < 0 && minDistances.up > distance) {
         closest.up = otherId;
-        minDistances.up = distanceUnknowNodo;
+        minDistances.up = distance;
       }
-      if (dyUnknowNodo > 0 && minDistances.down > distanceUnknowNodo) {
+      if (dy > 0 && minDistances.down > distance) {
         closest.down = otherId;
-        minDistances.down = distanceUnknowNodo;
+        minDistances.down = distance;
       }
 
-      if (dxUnknowNodo < 0 && minDistances.left > distanceUnknowNodo) {
+      if (dx < 0 && minDistances.left > distance) {
         closest.left = otherId;
-        minDistances.left = distanceUnknowNodo;
+        minDistances.left = distance;
       }
-      if (dxUnknowNodo > 0 && minDistances.right > distanceUnknowNodo) {
+      if (dx > 0 && minDistances.right > distance) {
         closest.right = otherId;
-        minDistances.right = distanceUnknowNodo;
+        minDistances.right = distance;
       }
     });
 
-    mapClosestNodo[nodoId] = { ...closest };
+    mapClosestNodo[nodoId] = closest;
   });
 
   return mapClosestNodo;
